13e0e3d61f1654f5c75ffc43382e7b6abfe363c1
Panos <panagiotisban@gmail.com>
2023-11-15 22:07:05 +0100

    Fix a mistake from co-pilot

--- weak_supervision/train_model.sh
+++ weak_supervision/train_model.sh
@@ -4,9 +4,9 @@ CONFIG_PATH="./config.ini"
 DATA_FILE_PATH="data_for_word2vec.gz"
 
 python -c "
-from your_python_script import Word2VecModelCreator
+from train_word2vec import Word2VecWrapper
 
-model_creator = Word2VecModelCreator('$DATA_FILE_PATH', '$CONFIG_PATH')
+model_creator = Word2VecWrapper('$DATA_FILE_PATH', '$CONFIG_PATH')
 model_creator.load_data()
 model_creator.clean_data()
 model_creator.lemmatize_and_remove_stopwords()
